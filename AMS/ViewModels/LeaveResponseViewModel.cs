d3e47a05be9f43615f160e3a86e948bb07577315
arslan5423 <sayhi.arslan>
2024-03-30 17:16:36 +0500

    viewmodel added

---
+++ AMS/ViewModels/LeaveResponseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using AMS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AMS.ViewModels
+{
+    public class LeaveResponseViewModel
+    {
+        public ReceivedLeaveRequests ReceivedLeaveRequests { get; set; }
+        public LeaveResponse LeaveResponse { get; set; }
+    }
+}
