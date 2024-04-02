51ae6a65b1ea75f532da17a1fe3ea3dd9c37444d
arslan5423 <sayhi.arslan>
2024-04-02 22:11:02 +0500

    new models

---
+++ AMS/Models/EmployeeHasMeeting.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AMS.Models
+{
+    public class EmployeeHasMeeting
+    {
+        public int Id { get; set; }
+        public Meeting Meeting { get; set; }
+        [ForeignKey("Meeting")]
+        [Display(Name = "Meeting")]
+        public int meetingId { get; set; }
+
+        public ApplicationUser ApplicationUser { get; set; }
+        [ForeignKey("ApplicationUser")]
+        [Display(Name = "Employee")]
+        public string employeeId { get; set; }
+    }
+}
---
+++ AMS/Models/Meeting.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AMS.Models
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please provide meeting date")]
+        [Display(Name = "Date")]
+        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Please provide meeting start time")]
+        [Display(Name = "Start Time")]
+        public TimeSpan StartTime { get; set; }
+
+        [Required(ErrorMessage = "Please provide meeting end time")]
+        [Display(Name = "End Time")]
+        public TimeSpan EndTime { get; set; }
+
+        [Required(ErrorMessage = "Please provide meeting agenda")]
+        public string Agenda { get; set; }
+
+        [Required(ErrorMessage = "Please provide meeting location")]
+        public string Location { get; set; }
+    }
+}
